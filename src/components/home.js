ce9117659d20ba40bc388c6e28a7748b84c84437
smk627751 <smk627751@gmail.com>
2023-07-20 21:13:32 +0530

    first commit

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,7 @@ function Home()
     return (
         <div className="home">
             <img src={data.image} className={'image'} alt="avatar"/>
-            <h1>Welcome back! {data.username}</h1>
+            <h1 className="heading">Welcome back! {data.username}</h1>
         </div>
     )
 }
